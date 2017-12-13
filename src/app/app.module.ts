101461eb456ab16870d4fffb48bdadb88b612c7e
Viet Tu <viettu@outlook.com>
2017-12-13 14:14:15 +0700

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/systemjs.config.js
@@ -0,0 +1,58 @@
+﻿/**
+ * System configuration for Angular samples
+ * Adjust as necessary for your application needs.
+ */
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': '/node_modules/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      'app': 'app',
+
+      // angular bundles
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+       
+        // angular material
+      '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
+      '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
+
+      // CDK individual packages (working with @angular/material)
+      '@angular/cdk/platform': 'npm:@angular/cdk/bundles/cdk-platform.umd.js',
+      '@angular/cdk/a11y': 'npm:@angular/cdk/bundles/cdk-a11y.umd.js',
+
+      // other libraries
+      'rxjs': 'npm:rxjs',
+
+      // working with @angular/material
+      'hammerjs': 'npm:hammerjs'
+
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        defaultExtension: 'js',
+        //meta: {
+        //  './*.js': {
+        //    loader: 'systemjs-angular-loader.js'
+        //  }
+        //}
+      },
+      rxjs: {
+        defaultExtension: 'js'
+      },
+      hammerjs: {
+          defaultExtension: 'js'
+      }
+    }
+  });
+})(this);
